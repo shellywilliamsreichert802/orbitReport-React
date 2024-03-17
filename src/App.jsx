033929846aa1c46f61ad34b90a4135f735269b9f
shellywilliamsreichert802 <154470623+shellywilliamsreichert802@users.noreply.github.com>
2024-03-17 12:04:21 -0500

    GA6 - 9 Passed - no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import { useState } from "react";
+import satData from "./components/satData";
 
 
 function App() {
+// Create the sat variable using useState and initialize it with satData
+  const [sat, setSat] = useState(satData);
+// Create the displaySats variable using Set to get unique orbitTypes
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+// Create the filterByType arrow function
+  const filterByType = (currentType) => {
+        const filteredSats = satData.filter((newSatDisplay) => {
+            return newSatDisplay.orbitType === currentType;
+        });
+        setSat(filteredSats);
+    };
+
+    // Rest of your code...
   return (
-    <div>
-      <Banner/>
-      <Buttons />
-      <Table />
-    </div>
+      // JSX components
+    // <div>
+      <>
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
+    </>
+    // </div>
   );
 }
 
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,16 @@
+import satData from "./satData";
+
+// Pass the props created from the App() function: filterByType, setSat, and displaySats.
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+        {displaySats.map((sat, id) => (
+           <button onClick={() => filterByType(sat)} key={id}>
+              {sat} Orbit
+            </button>
+          ))}
+      <button onClick={() => setSat(satData)}>All Orbit</button>
+{/* //code continues */}
     </div>
   );
 };
