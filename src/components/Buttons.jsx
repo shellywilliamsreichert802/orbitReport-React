21c0a72cec44a080c7d85a31c613844dad29e147
shellywilliamsreichert802 <154470623+shellywilliamsreichert802@users.noreply.github.com>
2024-03-17 12:20:52 -0500

    GA6 - 9 tests passed - with standard CSS

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,11 @@
 import satData from "./satData";
+import "./styling.css";
+
 
 // Pass the props created from the App() function: filterByType, setSat, and displaySats.
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
+    <div className = "flex-container">
         {displaySats.map((sat, id) => (
            <button onClick={() => filterByType(sat)} key={id}>
               {sat} Orbit
